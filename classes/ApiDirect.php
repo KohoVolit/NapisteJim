4d35b4ce85338bf60a26b10806963c28e46ae038
giro <jaroslav.semancik@kohovolit.eu>
2011-05-21 02:39:27 +0200

    added class ApiDirect

---
+++ classes/ApiDirect.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace KV;
+
+/**
+ * ...
+ */
+class ApiDirect
+{
+	/// KohoVolit.eu project to access through this API client
+	private $project;
+
+	/// default search params - parameters to include to each API resource request
+	private $default_params;
+
+	/// default data - data to include to each API resource request
+	private $default_data;
+
+	/**
+	 * ...
+	 */
+	public function __construct($project = 'kohovolit', $default_params = null, $default_data = null)
+	{
+		$this->project = $project;
+		$this->default_params = $default_params;
+		$this->default_data = $default_data;
+	}
+
+	/**
+	 * ...
+	 */
+	public function read($resource, $params = null)
+	{
+		$this->includeApiResourceClass($resource);
+		$full_params = (array)$params + (array)$this->default_params;
+		return $resource::read($full_params);
+	}
+
+	/**
+	 * ...
+	 */
+	public function create($resource, $data = null)
+	{
+		$this->includeApiResourceClass($resource);
+		$full_data = (array)$data + (array)$this->default_data;
+		return $resource::create($full_data);
+	}
+
+	/**
+	 * ...
+	 */
+	public function update($resource, $params = null, $data = null)
+	{
+		$this->includeApiResourceClass($resource);
+		$full_params = (array)$params + (array)$this->default_params;
+		$full_data = (array)$data + (array)$this->default_data;
+		return $resource::update($full_params, $full_data);
+	}
+
+	/**
+	 * ...
+	 */
+	public function delete($resource, $params = null)
+	{
+		$this->includeApiResourceClass($resource);
+		$full_params = (array)$params + (array)$this->default_params;
+		return $resource::delete($full_params);
+	}
+
+	/**
+	 * ...
+	 */
+	private function includeApiResourceClass($resource)
+	{
+		require_once  API_ROOT . '/conf/settings.php';
+		@include_once API_ROOT . "/projects/{$this->project}/conf/settings.php";
+		$ok = @include_once "projects/{$this->project}/api/$resource.php";
+		if (!$ok)
+			throw new \Exception("There is no API resource <em>$resource</em>.", 404);
+	}
+}
+
+?>
